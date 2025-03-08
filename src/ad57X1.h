e66e3d7db5021a21dbcdfba5febd11bddc8f6cdf
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2025-03-08 12:17:38 +0100

    Refactored the code and marked a lot of variables constant to improve compiler optimizations (#9)

--- src/ad57X1.h
+++ src/ad57X1.h
@@ -12,29 +12,29 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef __ARDUINO_AD57X1
-#define __ARDUINO_AD57X1
+#ifndef ARDUINO_AD57X1
+#define ARDUINO_AD57X1
 
-#include <stdint.h>    // uint8_t, etc.
+#include <cstdint>    // uint8_t, etc.
 // include the SPI library:
 #include <SPI.h>
 
 class AD57X1 {
   public:
     AD57X1(uint8_t cs_pin, SPIClass* _spi, uint8_t VALUE_OFFSET, uint32_t spiClockFrequency, int16_t ldac_pin, bool cs_polarity);
-    void setValue(uint32_t value);
-    uint32_t readValue();
-    uint32_t readControlRegister();
+    void setValue(uint32_t value) const;
+    [[nodiscard]] uint32_t readValue() const;
+    [[nodiscard]] uint32_t readControlRegister() const;
     void enableOutput();
     void setOffsetBinaryEncoding(bool enable);
     void setInternalAmplifier(bool enable);
     void setOutputClamp(bool enable);
     void setTristateMode(bool enable);
     void setReferenceInputRange(bool enable);
-    void updateControlRegister();
-    void setClearCodeValue(uint32_t value);
+    void updateControlRegister() const;
+    void setClearCodeValue(uint32_t value) const;
     void reset();
-    void begin(bool initSpi=true);
+    void begin(bool initSpi=true) const;
 
   private:
     const uint8_t VALUE_OFFSET;
@@ -42,33 +42,33 @@ class AD57X1 {
     
   protected:
     // DAC read/write mode
-    static const uint32_t WRITE_REGISTERS = 0UL << 23;
-    static const uint32_t READ_REGISTERS = 1UL << 23;
+    static constexpr uint32_t WRITE_REGISTERS = 0UL << 23;
+    static constexpr uint32_t READ_REGISTERS = 1UL << 23;
 
     // DAC value register (p. 21)
-    static const uint32_t DAC_REGISTER = 0b001UL << 20;
+    static constexpr uint32_t DAC_REGISTER = 0b001UL << 20;
 
     // Control register (p. 22)
-    static const uint32_t CONTROL_REGISTER = 0b010UL << 20;
-    static const uint8_t RBUF_REGISTER = 1;
-    static const uint8_t OUTPUT_CLAMP_TO_GND_REGISTER = 2;
-    static const uint8_t OUTPUT_TRISTATE_REGISTER = 3;
-    static const uint8_t OFFSET_BINARY_REGISTER = 4;
-    static const uint8_t SDO_DISABLE_REGISTER = 5;
-    static const uint8_t LINEARITY_COMPENSATION_REGISTER = 6;
+    static constexpr uint32_t CONTROL_REGISTER = 0b010UL << 20;
+    static constexpr uint8_t RBUF_REGISTER = 1;
+    static constexpr uint8_t OUTPUT_CLAMP_TO_GND_REGISTER = 2;
+    static constexpr uint8_t OUTPUT_TRISTATE_REGISTER = 3;
+    static constexpr uint8_t OFFSET_BINARY_REGISTER = 4;
+    static constexpr uint8_t SDO_DISABLE_REGISTER = 5;
+    static constexpr uint8_t LINEARITY_COMPENSATION_REGISTER = 6;
 
-    static const uint32_t REFERENCE_RANGE_10V = 0b0000;
-    static const uint32_t REFERENCE_RANGE_20V = 0b1100;
+    static constexpr uint32_t REFERENCE_RANGE_10V = 0b0000;
+    static constexpr uint32_t REFERENCE_RANGE_20V = 0b1100;
 
     // Clearcode register (p. 22)
-    static const uint32_t CLEARCODE_REGISTER = 0b011UL << 20;
+    static constexpr uint32_t CLEARCODE_REGISTER = 0b011UL << 20;
 
     // Software control register (p. 23)
-    static const uint32_t SW_CONTROL_REGISTER = 0b100UL << 20;
-    static const uint8_t SW_CONTROL_LDAC = 0b001;
+    static constexpr uint32_t SW_CONTROL_REGISTER = 0b100UL << 20;
+    static constexpr uint8_t SW_CONTROL_LDAC = 0b001;
 
-    void writeSPI(uint32_t value);
-    uint32_t readSPI(uint32_t value);
+    void writeSPI(uint32_t value) const;
+    [[nodiscard]] uint32_t readSPI(uint32_t value) const;
 
     const uint8_t PIN_CS;   // The ~Chip select pin used to address the DAC
     const int16_t PIN_LDAC;   // The ~LDAC select pin used to address the DAC
